1d1d45beba029f4f772e5de2251b8c301707f252
Ranjith-developer-dev <ranjithraffiqjob@gmail.com>
2024-03-20 08:17:51 +0530

    basic lambda setrup on aws-cdk Done

--- bin/aws-cdk.ts
+++ bin/aws-cdk.ts
@@ -2,10 +2,19 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { S3BucketStack } from '../lib/aws-s3-cdk-stack';
+import { BasicLambdaStack } from '../lib/aws-lambda-stack';
 
 const app = new cdk.App();
+
+//deploy basic s3 bucket
 const s3_bucket_stack = new S3BucketStack(app, 'ranjithS3Stack');
 
 //reusing assets
 const bucket = s3_bucket_stack.bucket;
+
+
+//deploy basic lambda function
+const basic_lambda_stack = new BasicLambdaStack(app, 'ranjithLambdaStack');
+
+
 
---
+++ lib/aws-lambda-stack.ts
@@ -0,0 +1,23 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+
+export class BasicLambdaStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        // The code that defines your stack goes here
+        const function_name = 'ranjith-cdk-lambda';
+        const lambda_path = 'src/lambda/basic_lambda';
+
+        const handler = new lambda.Function(this, function_name, {
+            functionName: function_name,
+            runtime: lambda.Runtime.NODEJS_20_X,
+            code: lambda.Code.fromAsset(lambda_path),
+            handler: 'lambda_function.handler',
+            timeout: cdk.Duration.minutes(10),
+            memorySize: 1024
+        });
+
+    }
+}
